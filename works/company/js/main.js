43bebcf93122547a0aabcc53d91be5285af88c54
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2021-01-26 21:48:41 +0300

    fix

--- works/company/js/main.js
+++ works/company/js/main.js
@@ -45,5 +45,6 @@ $("#polzunok").slider({
             $(".polzunok__window").eq(0).html(ui.value + "$");
             $(".polzunok__window").eq(0).css("left", $(".ui-slider-handle").position().left-30);
             $(".block__2__1__first__main__input__i").eq(1).html(ui.value * 1.3);
-        }
+        },
     });   
+    $('#polzunok').draggable();
